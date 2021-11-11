7f39c39f25d2d8dca1115a4ca0ff2ef118b25895
João Fernandes <joao.fernandesds26@gmail.com>
2021-11-10 21:31:35 -0300

    removida o redimensionamento de imagem.

--- back_end/recognition/utils.py
+++ back_end/recognition/utils.py
@@ -6,15 +6,15 @@ import numpy as np
 from PIL import Image
 
 
-def binary2image(binary):
-    return Image.open(io.BytesIO(binary))
-
-
 def rgba2rgb(image):
     image_rgb = Image.new('RGB', image.size, (255, 255, 255))
     image_rgb.paste(image, mask=image.getchannel('A'))
 
     return image_rgb
+
+
+def binary2image(binary):
+    return Image.open(io.BytesIO(binary))
 
 
 def image2array(image):
@@ -30,13 +30,6 @@ def image_extension(content_type: str):
 
 
 def number_of_faces(image):
-    width, height = image.size
+    face_locations = face_recognition.face_locations(image2array(image))
 
-    if width > 450 or height > 450:
-        ratio = width / height
-        new_size = (int(450 * ratio), 450)
-        image = image.resize(new_size)
-
-    face_bounding_boxes = face_recognition.face_locations(image2array(image))
-
-    return len(face_bounding_boxes)
+    return len(face_locations)
